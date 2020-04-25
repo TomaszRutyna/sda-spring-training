f723b24f152ef8fded8e147eff764467d3cb9151
Tomasz.Rutyna <Poniatowa2021!>
2020-04-25 13:51:20 +0200

    global error handling

--- src/main/java/pl/sda/sdaspringtraining/api/CarRentExceptionHandler.java
+++ src/main/java/pl/sda/sdaspringtraining/api/CarRentExceptionHandler.java
@@ -2,11 +2,13 @@ package pl.sda.sdaspringtraining.api;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import pl.sda.sdaspringtraining.NotFoundException;
 
 @ControllerAdvice
 public class CarRentExceptionHandler {
 
+    @ResponseBody
     @ExceptionHandler(value = NotFoundException.class)
     public String handleNotExist(NotFoundException ex) {
         return "Podany przez Ciebie obiekt nie istnieje, wiadomość serwera: " + ex.getMessage();
