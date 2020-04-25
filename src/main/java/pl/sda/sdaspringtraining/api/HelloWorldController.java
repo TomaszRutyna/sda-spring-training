d6e06e26621472d3dcff04d156080d981b74a1b0
Tomasz.Rutyna <Poniatowa2021!>
2020-04-25 11:52:35 +0200

    Parameters from request

--- src/main/java/pl/sda/sdaspringtraining/api/HelloWorldController.java
+++ src/main/java/pl/sda/sdaspringtraining/api/HelloWorldController.java
@@ -1,6 +1,8 @@
 package pl.sda.sdaspringtraining.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,15 @@ public class HelloWorldController {
     @GetMapping("/hello")
     String helloWordCommunicate() {
         return "Hello world!";
+    }
+
+    @GetMapping("/hello/{name}")
+    String helloWorldWithName(@PathVariable String name) {
+        return "Hello world, " + name;
+    }
+
+    @GetMapping("/multiply")
+    String multiply(@RequestParam("f1") Integer factorOne, @RequestParam("f2") Integer factorTwo) {
+        return "Wynik mnożenia to " + (factorOne * factorTwo);
     }
 }
