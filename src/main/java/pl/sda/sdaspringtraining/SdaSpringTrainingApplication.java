60c501dee1251a1ce1277a4ff38a6bb41385d13e
Tomasz.Rutyna <Poniatowa2021!>
2020-04-25 09:50:49 +0200

    Project initial

---
+++ src/main/java/pl/sda/sdaspringtraining/SdaSpringTrainingApplication.java
@@ -0,0 +1,12 @@
+package pl.sda.sdaspringtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaSpringTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdaSpringTrainingApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/sda/sdaspringtraining/SdaSpringTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.sdaspringtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaSpringTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
