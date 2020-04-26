183f54843b164c82c7c57e5333c65f3d11b65077
Tomasz.Rutyna <Poniatowa2021!>
2020-04-26 10:55:32 +0200

    Car repository

--- src/main/java/pl/sda/sdaspringtraining/service/CarService.java
+++ src/main/java/pl/sda/sdaspringtraining/service/CarService.java
@@ -6,6 +6,7 @@ import pl.sda.sdaspringtraining.api.model.Car;
 import pl.sda.sdaspringtraining.api.model.NewCar;
 import pl.sda.sdaspringtraining.api.model.UpdateCar;
 import pl.sda.sdaspringtraining.domain.CarEntity;
+import pl.sda.sdaspringtraining.domain.CarRepository;
 
 import java.rmi.NoSuchObjectException;
 import java.util.*;
@@ -14,47 +15,43 @@ import java.util.stream.Collectors;
 @Service
 public class CarService {
 
-    private static Integer carId = 0;
-    private static List<CarEntity> cars = new ArrayList<>();
+    private CarRepository carRepository;
+
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
 
     public void createCar(NewCar newCar) {
-        cars.add(new CarEntity(carId++, newCar.getRegisterPlate(), newCar.getProducer(),
-                newCar.getModel(), newCar.getYearOfProduction(), String.join(",", newCar.getOptions())));
+        CarEntity entity = new CarEntity(null, newCar.getRegisterPlate(), newCar.getProducer(),
+                newCar.getModel(), newCar.getYearOfProduction(), String.join(",", newCar.getOptions()));
+
+        carRepository.save(entity);
     }
 
     public void updateCar(UpdateCar updateCar) {
-        for (CarEntity entity : cars) {
-            if (entity.getId() == updateCar.getId()) {
-                entity.setRegisterPlate(updateCar.getRegisterPlate());
-                entity.setOptions(String.join(",", updateCar.getOptions()));
-                return;
-            }
-        }
-        throw new NotFoundException("Car with id " + updateCar.getId() + " not exist");
+        CarEntity carToUpdate = carRepository.findById(updateCar.getId())
+                .orElseThrow(() -> new NotFoundException("Car with id " + updateCar.getId() + " not exist"));
+
+        carToUpdate.setRegisterPlate(updateCar.getRegisterPlate());
+        carToUpdate.setOptions(String.join(",", updateCar.getOptions()));
+
+        carRepository.save(carToUpdate);
     }
 
     public Optional<Car> getById(Integer id) {
-        return cars.stream().filter(cus -> cus.getId() == id)
-                .findFirst()
+        return carRepository.findById(id)
                 .map(ent -> new Car(ent.getId(), ent.getRegisterPlate(), ent.getProducer(),
                         ent.getModel(), ent.getYearOfProduction(), Arrays.asList(ent.getOptions().split(","))));
     }
 
     public List<Car> getAll() {
-        return cars.stream()
+        return carRepository.findAll().stream()
                 .map(ent -> new Car(ent.getId(), ent.getRegisterPlate(), ent.getProducer(),
                         ent.getModel(), ent.getYearOfProduction(), Arrays.asList(ent.getOptions().split(","))))
                 .collect(Collectors.toList());
     }
 
     public void deleteById(Integer id) {
-        Iterator<CarEntity> carIterator = cars.iterator();
-        while (carIterator.hasNext()) {
-            CarEntity next = carIterator.next();
-            if (next.getId() == id) {
-                carIterator.remove();
-                return;
-            }
-        }
+        carRepository.deleteById(id);
     }
 }
