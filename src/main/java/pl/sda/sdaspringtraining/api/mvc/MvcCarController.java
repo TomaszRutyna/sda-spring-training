9da79c2527b20ef06a8b278de9796d8b9e44c16d
Tomasz.Rutyna <Poniatowa2021!>
2020-05-16 12:44:29 +0200

    Pętli w thymleaf

--- src/main/java/pl/sda/sdaspringtraining/api/mvc/MvcCarController.java
+++ src/main/java/pl/sda/sdaspringtraining/api/mvc/MvcCarController.java
@@ -1,7 +1,26 @@
 package pl.sda.sdaspringtraining.api.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import pl.sda.sdaspringtraining.service.CarService;
 
 @Controller
+@RequestMapping("/cars")
 public class MvcCarController {
+
+    private final CarService carService;
+
+    public MvcCarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping
+    ModelAndView showAllCars() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("cars/cars.html");
+        mav.addObject("cars", carService.getAll());
+        return mav;
+    }
 }
