e18d4c0469639c6c0fa13c3d379d1b7fc5452f97
Tomasz.Rutyna <Poniatowa2021!>
2020-05-16 13:44:42 +0200

    Przekierowania na stronach Thymleaf

--- src/main/java/pl/sda/sdaspringtraining/api/mvc/MvcCarController.java
+++ src/main/java/pl/sda/sdaspringtraining/api/mvc/MvcCarController.java
@@ -2,6 +2,7 @@ package pl.sda.sdaspringtraining.api.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import pl.sda.sdaspringtraining.service.CarService;
@@ -21,6 +22,14 @@ public class MvcCarController {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("cars/cars.html");
         mav.addObject("cars", carService.getAll());
+        return mav;
+    }
+
+    @GetMapping("/{id}")
+    ModelAndView showCarDetails(@PathVariable Integer id) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("cars/carDetails.html");
+        mav.addObject("car", carService.getById(id).get());
         return mav;
     }
 }
