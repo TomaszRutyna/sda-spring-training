5bb50e7c86097b26797f21877aac0d9c0013e850
Tomasz.Rutyna <Poniatowa2021!>
2020-05-16 11:54:11 +0200

    Kontrolery MVC

--- src/main/java/pl/sda/sdaspringtraining/api/mvc/MvcMainPageController.java
+++ src/main/java/pl/sda/sdaspringtraining/api/mvc/MvcMainPageController.java
@@ -2,13 +2,23 @@ package pl.sda.sdaspringtraining.api.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
 
 @Controller
 public class MvcMainPageController {
 
     @GetMapping("/")
-    String mainPage() {
-        return "main.html";
+    ModelAndView mainPage(@RequestParam String branch) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("main.html");
+
+        mav.addObject("branch", branch);
+        mav.addObject("now", LocalDate.now().toString());
+        return mav;
     }
 
 }
