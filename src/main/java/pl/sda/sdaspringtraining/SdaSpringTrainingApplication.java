979e8c1f18e03252e8c287f61d9488855ceabff7
Tomasz.Rutyna <Poniatowa2021!>
2020-05-17 12:52:12 +0200

    Operacje schedulowane - adnotacja Scheduled

--- src/main/java/pl/sda/sdaspringtraining/SdaSpringTrainingApplication.java
+++ src/main/java/pl/sda/sdaspringtraining/SdaSpringTrainingApplication.java
@@ -3,8 +3,10 @@ package pl.sda.sdaspringtraining;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import pl.sda.sdaspringtraining.config.RentProperties;
 
+@EnableScheduling
 @EnableConfigurationProperties({RentProperties.class})
 @SpringBootApplication
 public class SdaSpringTrainingApplication {
---
+++ src/main/java/pl/sda/sdaspringtraining/service/SchedulingExampleService.java
@@ -0,0 +1,14 @@
+package pl.sda.sdaspringtraining.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SchedulingExampleService {
+
+/*    @Scheduled(fixedRate = 1000)
+    public void scheduledTask() {
+        System.out.println("*******************");
+        System.out.println("HELLO WORLD!");
+    }*/
+}
